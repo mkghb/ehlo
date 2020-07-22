daf0d45530d9eef4a486b254f25ae7dbbf2569c7
Hammerhead <hammerhead@Neo.local>
2020-07-22 17:11:07 +0530

    added o365 cmd

--- o365cmd/msonline.ps1
+++ o365cmd/msonline.ps1
@@ -8,3 +8,8 @@ Get-MsolAccountSku
 Get-MsolUser -All -UnlicensedUsersOnly
 Set-MsolUserLicense -UserPrincipalName "cbp@iibcouncil.org" -AddLicenses "ECCouncilAbq:ENTERPRISEPACK"
 Set-MsolUserLicense -UserPrincipalName "cbp@iibcouncil.org" -AddLicenses "ECCouncilAbq:EMS"
+
+
+
+
+Get-Mailbox | Get-ADPermission | Where-Object {($_.ExtendedRights -like 'Send-As') -and ($_.User -notlike 'NT AUTHORITY\SELF')} | fl Identity,User,ExtendedRights,AccessRights -wrap
